239c350f10dbedcf44603577d6eaca522fdfeab9
Ashkeyev Ali <ashkeyevli@gmail.com>
2020-10-08 00:25:57 +0600

    comments added

--- blockchain/main/urls.py
+++ blockchain/main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from main.views import index, get_usd
 
 urlpatterns = [
- path('', index),
- path('answear', get_usd, name='get_usd')
+ path('', index),#url path=s to main html
+ path('answear', get_usd, name='get_usd')#url path to answear pagw=
  ]
--- blockchain/main/views.py
+++ blockchain/main/views.py
@@ -3,33 +3,33 @@ import requests
 
 
 # Create your views here.
-API_KEY = 'c2d9966a-0e29-4251-82ac-59b98a2c8598'
-URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+API_KEY = 'c2d9966a-0e29-4251-82ac-59b98a2c8598'#API key for website coinmarket
+URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'#url to access latest cryptocurrency prices and information
 headers = {'Accepts': 'application/json',
-      'X-CMC_PRO_API_KEY': API_KEY}
+      'X-CMC_PRO_API_KEY': API_KEY}#header with API key for coinmarket website
 
 
-def index(request):
+def index(request):# function for representing start page index.html
     context = {}
-    return render(request, 'index.html', context)
+    return render(request, 'index.html', context)#The template is executed with the passed word of text and returns an HttpResponse with the content received.
 
-def get_usd(request):
+def get_usd(request):#function for converting etherium to usd
     try:
-        amount = float(request.GET['amount'])
-        response =  requests.get(URL, headers = headers)
-        response_json = response.json()
-        result = round(amount*response_json['data'][1]['quote']['USD']['price'],3)
+        amount = float(request.GET['amount'])#getting amount of etherium from input in index.html
+        response =  requests.get(URL, headers = headers)#getting response from coinmarketcap website with all cryptocurrency information
+        response_json = response.json()#getting json of response
+        result = round(amount*response_json['data'][1]['quote']['USD']['price'],3)# selecting from json etherium price for USD, multipling it to the entered amount and round it to 3 digits.
         mydictionary = {
-            "amount": amount,
-            "answer": result,
-            "error" : False,
-            "result" : True
+            "amount": amount,#setting for dictionary amount entered
+            "answer": result,#setting result with converted etherium to USD
+            "error" : False,#setting no error
+            "result" : True#setting result boolean true
         }
-        return render(request, "index.html", context = mydictionary)
+        return render(request, "index.html", context = mydictionary)#returns HttpResponse with the content
     except:
         mydictionary = {
-            "error" : True,
-            "result" : False
+            "error" : True,#if was error set bool error true
+            "result" : False#no result
         }
-        return render(request, 'index.html', context=mydictionary)
+        return render(request, 'index.html', context=mydictionary)#returns HttpResponse with setted dictionary
 
